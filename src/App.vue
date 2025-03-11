fd7a194a659a82d7013da114b0763912db83efa6
Leonardo Vargas <leonardo.vargas@example.com>
2025-03-11 00:46:22 -0500

    feat: adding new dependencies

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-
+      <span class="bg-red-500">Color Negro</span>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
